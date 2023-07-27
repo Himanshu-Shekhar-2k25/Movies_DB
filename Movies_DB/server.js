f18043a38cd7b4e85fb277e2e1a4a73c4459e80c
Himanshu Shekhar <111629292+Himanshu-Shekhar-2k25@users.noreply.github.com>
2023-07-28 00:09:19 +0530

    Update server.js

--- Movies_DB/server.js
+++ Movies_DB/server.js
@@ -14,23 +14,20 @@ app.get('/',function(req,res)
 
 app.post('/',function(req,res)
 {
-//     const movie = req.body.movie_name
-//     const year = req.body.yearOfRelease
-//     const apikey = '5aa6ab5e'
-//     const url = 'http://www.omdbapi.com/?t='+movie+'&y='+year+'&plot=full&apikey=5aa6ab5e'
+    const movie = req.body.movie_name
+    const year = req.body.yearOfRelease
+    const apikey = '5aa6ab5e'
+    const url = 'http://www.omdbapi.com/?t='+movie+'&y='+year+'&plot=full&apikey=5aa6ab5e'
 
-//     http.get(url, function(response)
-//     {
-//         response.on("data",function(data)
-//         {
-//             data = JSON.parse(data)
+    http.get(url, function(response)
+    {
+        response.on("data",function(data)
+        {
+            data = JSON.parse(data)
                        
-//             res.render('Movie',{image:data.Poster,title:data.Title,year:data.Year,release:data.Released,runtime:data.Runtime,genre:data.Genre,director:data.Director,writer:data.Writer,actors:data.Actors,plot:data.Plot,language:data.Language,country:data.Country,awards:data.Awards,ratings:data.Ratings[0].Value,imdb_ratings:data.imdbRating,boxoffice:data.BoxOffice})
-//         })
-//     })
-// })
-
-    res.send("Hello");
+            res.render('Movie',{image:data.Poster,title:data.Title,year:data.Year,release:data.Released,runtime:data.Runtime,genre:data.Genre,director:data.Director,writer:data.Writer,actors:data.Actors,plot:data.Plot,language:data.Language,country:data.Country,awards:data.Awards,ratings:data.Ratings[0].Value,imdb_ratings:data.imdbRating,boxoffice:data.BoxOffice})
+        })
+    })
 })
 
 app.listen(3000,function()
