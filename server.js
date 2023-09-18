ef14a4eea43f292c9989604450962ae4d3290fbb
Himanshu Shekhar <111629292+Himanshu-Shekhar-2k25@users.noreply.github.com>
2023-09-18 21:32:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,46 +2,50 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const bodyParser = require('body-parser')
-const path = require('path')
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine','ejs')
-app.set('views','./views')
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
-app.get('/',function(req,res)
-{
-    const filePath = path.join(__dirname, 'index.html');
-    res.sendFile(filePath);
+app.get('/', function (req, res) {
+    res.sendFile("C:\\Users\\LENOVO\\Desktop\\Web Dev\\Movies_DB\\index.html")
 })
 
-app.post('/',function(req,res)
-{
+app.post('/', function (req, res) {
     const movie = req.body.movie_name
     const year = req.body.yearOfRelease
     const apikey = '5aa6ab5e'
-    const url = 'http://www.omdbapi.com/?t='+movie+'&y='+year+'&plot=full&apikey=5aa6ab5e'
-    
-    http.get(url, function(response)
-    {
-        response.on("data",function(data)
-        {
+    const url = 'http://www.omdbapi.com/?t=' + movie + '&y=' + year + '&plot=full&apikey=5aa6ab5e'
+
+    http.get(url, function (response) {
+        response.on("data", function (data) {
             data = JSON.parse(data)
-            
-            if(data.Response == 'False')
-            {
-                const filePath = path.join(__dirname, 'index.html');
-                res.sendFile(filePath);
+            if (data.Response != 'True') {
+                res.sendFile("C:\\Users\\LENOVO\\Desktop\\Web Dev\\Movies_DB\\index.html")
             }
-            else    
-            {
-                const filePath = path.join(__dirname, 'Movie.ejs');
-                res.render(filePath,{image:data.Poster,title:data.Title,year:data.Year,release:data.Released,runtime:data.Runtime,genre:data.Genre,director:data.Director,writer:data.Writer,actors:data.Actors,plot:data.Plot,language:data.Language,country:data.Country,awards:data.Awards,ratings:data.Ratings[0].Value,imdb_ratings:data.imdbRating,boxoffice:data.BoxOffice})
-            }
+            else
+                res.render('Movie', {
+                    image: data.Poster != null ? data.Poster:"N/A",
+                    title: data.Title != null ? data.Title : "N/A",
+                    year: data.Year != null ? data.Year : "N/A",
+                    release: data.Released != null ? data.Released : "N/A",
+                    runtime: data.Runtime != null ? data.Runtime : "N/A",
+                    genre: data.Genre != null ? data.Genre : "N/A",
+                    director: data.Director != null ? data.Director : "N/A",
+                    writer: data.Writer != null ? data.Writer : "N/A",
+                    actors: data.Actors != null ? data.Actors : "N/A",
+                    plot: data.Plot != null ? data.Plot : "N/A",
+                    language: data.Language != null ? data.Language : "N/A",
+                    country: data.Country != null ? data.Country : "N/A",
+                    awards: data.Awards != null ? data.Awards : "N/A",
+                    ratings: data.Ratings[0] != null ? data.Ratings[0].Value: "N/A",
+                    imdb_ratings: data.imdbRating != null ? data.imdbRating: "N/A",
+                    boxoffice: data.BoxOffice != null ? data.BoxOffice: "N/A"
+                })
         })
     })
 })
 
-app.listen(3000,function()
-{
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
